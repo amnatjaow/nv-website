8e3c4234fc3f59f2ce354800e7ea7c93fc19dd12
T1704@cct.com <T1704@cct.com>
2019-02-08 15:32:40 +0700

    Restapi use controller + route

--- server/src/app.js
+++ server/src/app.js
@@ -19,32 +19,6 @@ app.post('/hello', function (req, res) {
   res.send('OK you post - ' + req.body.name)
 })
 
-/* RESTFul Api for user management */
-// create user
-app.post('/user', function (req, res) {
-  res.send('สร้างผู้ใช้')
-})
-
-// edit user, suspend, active
-app.put('/user/:userId', function (req, res) {
-  res.send('แก้ไขผู้ใช้')
-})
-
-// delete user
-app.delete('/user/:userId', function (req, res) {
-  res.send('ลบผู้ใช้งาน')
-})
-
-// get user by id
-app.get('/user/:userId', function (req, res) {
-  res.send('ดูข้อมูลผู้ใช้งาน')
-})
-
-// get all user
-app.get('/users', function (req, res) {
-  res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
-})
-
 let port = 8000
 
 app.listen(port, function () {
